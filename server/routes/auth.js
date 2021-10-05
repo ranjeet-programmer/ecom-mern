ca16eb42824f1c0896d5b1433ddba2f49a66c3aa
revenge_ff <62922876+ranjeet-programmer@users.noreply.github.com>
2021-10-05 20:45:57 +0530

    section_5_done

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,9 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-// import controllers
+//  middlewares
+
+const { authCheck } = require("../middlewares/auth");
+//  controllers
 
 const { createOrUpdateUser } = require("../controllers/auth");
-router.get("/create-or-update-user", createOrUpdateUser);
+
+router.post("/create-or-update-user", authCheck, createOrUpdateUser);
 
 module.exports = router;
